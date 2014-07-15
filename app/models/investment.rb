c94522b14964878302ea8a66fbaeb6e468d89d36
Mikenzie Ginsberg <mginsberg24@gmail.com>
2014-07-15 15:12:49 +0200

    prepare to merge

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,5 +1,18 @@
 class Investment < ActiveRecord::Base
 	belongs_to :investor
 	has_many :series
+	validates :amount, presence: true
 
+	def self.create(investment_params) 
+		investment = Investment.new(investment_params)
+		investment_amount = investment_params[:amount].to_f
+
+		if investment_amount < 10000
+			return [false, "The minimum investment is $10,000", investment]
+		end
+
+		if investment.save!
+			return [true, "You have successfully invested.", investment]
+		end
+	end
 end
